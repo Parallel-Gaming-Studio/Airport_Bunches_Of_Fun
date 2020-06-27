c2847680cf20ec1c88177f06dbd132c601684f48
ChrisMedeiros42 <61912565+ChrisMedeiros42@users.noreply.github.com>
2020-06-27 22:11:15 +0400

    Removed Excess Background
    - scene_play.js had an extra game.playBackground, causing errors.

--- docs/scripts/scene_play.js
+++ docs/scripts/scene_play.js
@@ -927,28 +927,6 @@ game.playFieldGrid = {
 };
 
 // - Gems
-game.playBackground = {
-    // Get handle to image
-    image: document.getElementById("playBackground"),
-    // Declare object transform information
-    org_width: 1920 * game.scale,
-    org_heigth: 1080 * game.scale,
-    width: 0,
-    height: 0,
-    posX: 0,
-    posY: 0,
-    // Adjust the object's transform
-    resize: function () {
-        this.width = engine.width;
-        this.height = engine.height;
-    },
-    // Draw the object
-    draw: function () {
-        this.resize();
-        engine.context.drawImage(this.image, this.posX, this.posY, this.width, this.height);
-    }
-};
-
 game.gemTriangle = {
 	// Get handle to image
     image: document.getElementById("gemTriangle"),
